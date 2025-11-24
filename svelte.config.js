5fdc2900a12c554eec5dd0498f2f9480de8c3043
Samarth Verma <vsamarthj@gmail.com>
2025-11-25 00:58:42 +0800

    chore: use vercel adapter when building on vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,9 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapterAuto from "@sveltejs/adapter-auto";
+import adapterVercel from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+// Use Vercel adapter when building on Vercel, auto adapter otherwise
+const adapter = process.env.VERCEL ? adapterVercel : adapterAuto;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
