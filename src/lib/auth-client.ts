a70a2e49abad2656c959441b37dc5a7ca0dc5fd8
Samarth Verma <vsamarthj@gmail.com>
2025-11-27 17:18:13 +0800

    refactor: make auth-form generic

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -5,14 +5,15 @@ export const authClient = createAuthClient({});
 type ErrorTypes = Partial<Record<keyof typeof authClient.$ERROR_CODES, string>>;
 
 const errorTypes: ErrorTypes = {
-  INVALID_EMAIL_OR_PASSWORD: "Incorrect email or password.",
-  INVALID_TOKEN:
-    "This reset link is invalid or has expired. Please request a new one.",
+  INVALID_EMAIL_OR_PASSWORD: "Email or password is incorrect.",
+  INVALID_TOKEN: "This link is invalid or has expired. Request a new one.",
+  USER_ALREADY_EXISTS: "An account with this email already exists.",
+  USER_ALREADY_EXISTS_USE_ANOTHER_EMAIL: "This email is already taken. Try another one.",
 };
-
 export const getErrorMessage = (errorCode?: string) => {
   return (
     errorTypes[errorCode as keyof ErrorTypes] ||
     "Something went wrong. Please try again."
   );
 };
+  
