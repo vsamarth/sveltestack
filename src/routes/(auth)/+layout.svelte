7be8f7f35023ad75a7438226cf9d614e000d6c46
Alexandre Dubois <al.dubois@outlook.com>
2025-12-08 08:11:41 -0500

    feat: add dark mode support to the homepage; add toggle to homepage & auth layout.
    chore: rename class to linear from gradient in auth layout.

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import GalleryVerticalEndIcon from "@lucide/svelte/icons/gallery-vertical-end";
+  import ThemeToggle from "$lib/components/theme-toggle.svelte";
 
   let { children } = $props();
 </script>
 
 <div class="grid min-h-svh lg:grid-cols-2">
   <div class="flex flex-col gap-4 p-6 md:p-10">
-    <div class="flex justify-center gap-2 md:justify-start">
+    <div class="flex justify-between items-center">
       <a href="/" class="flex items-center gap-2 font-medium">
         <div
           class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"
@@ -15,6 +16,7 @@
         </div>
         Vault
       </a>
+      <ThemeToggle />
     </div>
     <div class="flex flex-1 items-center justify-center">
       <div class="w-full max-w-sm">
